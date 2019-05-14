6653532ee31f5ce22fab131742511587a660ee54
Alexander <jamsic@inbox.ru>
2019-05-14 20:09:23 +1000

    room_card (day_1)

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
 
-import '../ui_kit/room_search/room_search'
-import '../ui_kit/room_search/room_search.scss'
+// import '../ui_kit/room_search/room_search'
+// import '../ui_kit/room_search/room_search.scss'
+
+import '../ui_kit/room_card/room_card.pug'
+import '../ui_kit/room_card/room_card.scss'
